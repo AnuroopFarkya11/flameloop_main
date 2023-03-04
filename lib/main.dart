5e1f2198b131acb640893bfd457e75a578114aaf
bhavik <kotharibhavik23@gmail.com>
2023-03-04 23:57:02 +0530

    added some configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'package:flameloop/app/layouts/get_started.dart';
-import 'package:flameloop/app/layouts/splash_screen.dart';
 import 'package:flameloop/app/routes/route_path.dart';
 import 'package:flameloop/app/routes/routes.dart';
 import 'package:flutter/material.dart';
@@ -14,33 +12,20 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      debugShowCheckedModeBanner: false ,
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-
-
-
-
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
-            primary: Colors.white
+            backgroundColor: Colors.white,
           )
-          )
-        ),
-
-
-      // initialRoute: RouteClass.getSlashScreen(),
-
+        )
+      ),
       initialRoute: RoutePaths.splah_screen,
       getPages: RouteClass.routes,
-
-
-
     );
-
   }
 }
